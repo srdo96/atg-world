0c344ba498300e28ccfdb0cf0cc61a6fb536fc74
Sakib Rahman <seu.sakib@gmail.com>
2022-07-28 23:15:35 +0600

    add banner

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import NavigationBar from "./components/NavigationBar";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <NavigationBar />
+      <Home />
     </div>
   );
 }
